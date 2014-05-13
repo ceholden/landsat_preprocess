4db540487600c11be487eafdb67590b037572641
Chris Holden <ceholden@gmail.com>
2014-05-13 15:55:49 -0400

    Changed build script to output into 'build' folder

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@
 #
 ###
 
+if [ ! -d build ]; then
+    mkdir build
+fi
+
 for ipynb in $(find ./ -name '*ipynb'); do
     # Ignore checkpoints
     dn=$(basename $(dirname $ipynb))
@@ -12,11 +16,22 @@ for ipynb in $(find ./ -name '*ipynb'); do
         continue
     fi
 
+    # Basename minus extension of IPython notebook
+    f=$(basename $ipynb .ipynb)
+
     ipython nbconvert $ipynb
 #    ipython nbconvert --to=latex --post=PDF $ipynb
 
+    # Move built HTML and/or PDF to build/ directory
+    if [ -f ${f}.html ]; then
+        mv ${f}.html build/
+    fi
+    if [ -f ${f}.pdf ]; then
+        mv ${f}.pdf build/
+    fi
+
     # remove tex files
-    tex=$(basename $ipynb .ipynb).tex
+    tex=${f}.tex
     if [ -f $tex ]; then
         rm $tex
     fi
